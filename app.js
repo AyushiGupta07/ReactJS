6d75fb8965ad0e330a05c6a62dcdb64ba4471d32
Ayushi Gupta <Ayushi.Gupta@Geminisolutions.com>
2026-06-24 19:15:17 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,27 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello word from Ayushi",
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "I am an h2 tag")],
+  ),
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "I am an h2 tag")],
+  )],
+);
+
+root.render(parent);
+console.log(parent);
